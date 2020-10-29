21df9ffb247b6473ee9fa7fbb061444a59ea97a1
Jombuena <Takumakun@gmail.com>
2020-10-29 12:43:48 +0100

    added error model

---
+++ src/app/error-list/error-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Error } from '../models/error.model';
+
+@Component({
+  selector: 'app-error-list',
+  templateUrl: './error-list.component.html',
+  styleUrls: ['./error-list.component.scss']
+})
+export class ErrorListComponent implements OnInit {
+
+  errorList: Error[] = [
+    new Error('File not found', ['file.jpg', 'file_2.jpg', 'file_large.jpg']),
+    new Error('File exists', [
+      'file3.jpg',
+      'file_test.jpg',
+      'file_large.jpg',
+      'file_.jpg',
+    ])
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
---
+++ src/app/models/error.model.ts
@@ -0,0 +1,4 @@
+export class Error {
+  constructor(public name: string, public files: Array<string>) {
+  }
+}
